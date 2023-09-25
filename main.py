1b040c95dee8144709618b19a632f4d22b375bed
An Dang <dangthanhan507@gmail.com>
2023-09-24 21:47:05 -0400

    Add starting pos/orientation.

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import pybullet as p
 import pybullet_data
-
+import numpy as np
 
 if __name__ == '__main__':
     physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
     p.setAdditionalSearchPath(pybullet_data.getDataPath()) #used by loadURDF
     p.setGravity(0,0,-10)
-    planeId = p.loadURDF("urdf/digit_model.urdf")
+
+    #set start position
+
+    startPos = [0,0,1]
+    startOrientation = p.getQuaternionFromEuler([np.pi/2,0,0])
+
+    robotId = p.loadURDF("urdf/digit_model.urdf", startPos, startOrientation)
+
+
     p.stepSimulation()
     input()
-    p.disconnect()    
+    p.disconnect()
